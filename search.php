23b5b5b75a61c196ed52ec6ad994457fa74f6a1d
Tran Tien <trungtien.uit@gmail.com>
2018-03-10 11:31:22 +0700

    first commit

---
+++ search.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tìm kiếm</title>
+</head>
+<body>
+	 <!-- <div class="wrapform"
+		 <form action="1_submit" method="get" action="process.php" accept-charset="utf-8" class="form">
+			<select name="dropdown" >
+				<option value="TpHCM">TP Hồ Chí Minh</option>
+				<option value="HN">Hà Nội</option>				
+			</select>
+			<input type="submit" name="search" value="Search">
+		</form>  Hết form  -->
+<div class="wrapform">
+<form method="post" action="process.php">
+	<div class="form-group">
+          <label for="Địa Điểm">Nhập địa điểm</label>
+  <select name="taskOption">
+  	<option value="Cần Thơ">Cần Thơ</option>
+    <option value="Hải Phòng">Hải Phòng</option>
+    <option value="Hà Nội">Hà Nội</option>
+    <option value="TP HCM" selected="TP HCM">TP HCM</option>
+    <option value="Đà Nẵng">Đà Nẵng</option>
+    <option value="An Giang">An Giang</option>
+    <option value="Bà Rịa - Vũng Tàu">Bà Rịa - Vũng Tàu</option>
+    <option value="Bắc Giang">Bắc Giang</option>
+    <option value="Bắc Kạn">Bắc Kạn</option>
+    <option value="Bạc Liêu">Bạc Liêu</option>
+    <option value="Bắc Ninh">Bắc Ninh</option>
+    <option value="Bến Tre">Bến Tre</option>
+    <option value="Bình Định">Bình Định</option>
+    <option value="Bình Dương">Bình Dương</option>
+    <option value="Bình Phước">Bình Phước</option>
+    <option value="Bình Thuận">Bình Thuận</option>
+    <option value="Cà Mau">Cà Mau</option>
+    <option value="Cao Bằng">Cao Bằng</option>
+    <option value="Đắk Lắk">Đắk Lắk</option>
+    <option value="Đắk Nông">Đắk Nông</option>
+    <option value="Điện Biên">Điện Biêng</option>
+    <option value="Đồng Nai">Đồng Nai</option>
+    <option value="Đồng Tháp">Đồng Tháp</option>
+    <option value="Gia Lai">Gia Lai</option>
+    <option value="Hà Giang">Hà Giang</option>
+    <option value="Hà Nam">Hà Nam</option>
+    <option value="Hà Tĩnh">Hà Tĩnh</option>
+    <option value="Hải Dương">Hải Dương</option>
+    <option value="Hậu Giang">Hậu Giang</option>
+    <option value="Hòa Bình">Hòa Bình</option>
+    <option value="Hưng Yên">Hưng Yên</option>
+    <option value="Khánh Hòa">Khánh Hòa</option>
+    <option value="Kiên Giang">Kiên Giang	</option>
+    <option value="Kon Tum">Kon Tum</option>
+    <option value="Lai Châu">Lai Châu</option>
+    <option value="Lâm Đồng">Lâm Đồng</option>
+    <option value="Lạng Sơn">Lạng Sơn</option>
+    <option value="Lào Cai">Lào Cai</option>
+    <option value="Long An">Long An</option>
+    <option value="Nam Định">Nam Định</option>
+    <option value="Nghệ An">Nghệ An</option>
+    <option value="Ninh Bình">Ninh Bình</option>
+    <option value="Ninh Thuận">Ninh Thuận</option>
+    <option value="Phú Thọ">Phú Thọ</option>
+    <option value="Quảng Bình">Quảng Bình</option>
+    <option value="Quảng Nam">Quảng Nam</option>
+    <option value="Quảng Ngãi">Quảng Ngãi</option>
+    <option value="Quảng Ninh">Quảng Ninh</option>
+    <option value="Quảng Trị">Quảng Trị</option>
+    <option value="Sóc Trăng">Sóc Trăng</option>
+    <option value="Sơn La">Sơn La</option>
+    <option value="Tây Ninh">Tây Ninh</option>
+    <option value="Thái Bình">Thái Bình</option>
+    <option value="Thái Nguyên">Thái Nguyên</option>
+    <option value="Thanh Hóa">Thanh Hóa</option>
+    <option value="Thừa Thiên Huế">Thừa Thiên Huế</option>
+    <option value="Tiền Giang">Tiền Giang</option>
+    <option value="Trà Vinh">Trà Vinh</option>
+    <option value="Tuyên Quang">Tuyên Quang</option>
+    <option value="Vĩnh Long">Vĩnh Long</option>
+    <option value="Vĩnh Phúc">Vĩnh Phúc</option>
+    <option value="Yên Bái">Yên Bái</option>
+    <option value="Phú Yên">Phú Yên</option>       
+  </select>
+  <label for="Lịch trình">Nhập lịch trình</label>
+    <input type="date" name="date"/>
+       <input type="text" name="search">
+    <input type="submit" value="Submit the form"/>
+</div> <!-- hết form-group -->
+
+<div class="form-group">
+	
+</div><!-- hết form-group -->
+<div class="form-group">
+	<label for="Tính giá">Tính giá</label>
+	
+
+</div><!-- hết form-group -->
+
+</form>
+ 	</div> <!-- Hết wrapform -->
+</body>
+</html>
