3ba1de5b77e4f9416092bc81d7791fb67204db7d
Tran Tien <trungtien.uit@gmail.com>
2018-03-28 08:11:05 +0700

    review

--- func_update_automatic.php
+++ func_update_automatic.php
@@ -11,26 +11,28 @@ if(!(strcmp($result,"success"))){
 	RemoveAllPrice();
 	#region -- Update All 
 $provinces=GetAllProvince();
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Start>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_Dat_Ban($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_Nha_Ban>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_Nha_Ban($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_Nha_Ban>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_Dien_Thoai_May_Tinh($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_Dien_Thoai_May_Tinh>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_May_Quay_Phim($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_May_Quay_Phim>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_Ban_O_To_Cu($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_Ban_O_To_Cu>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Rao_Vat_Ban_Xe_May_Cu($provinces);
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Rao_Vat_Ban_Xe_May_Cu>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Cay_Canh();
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Cay_Canh>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Cay_Giong();
 // Hàm lấy dữ liệu 5 dịch vụ liên quan khách sạn
-
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Cay_Giong>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 Insert_Khach_San($provinces);
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Insert_Khach_San>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 #endregion
+Echo '>>>>>>>>>>>>>>>>>>>>>>>Finished>>>>>>>>>>>>>>>>>>>>>>>>>>>>';
 }
 
 ?>
